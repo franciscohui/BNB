0e3d23da470f49443c6e8bf22c98e77b89061d0d
Francisco Hui <francisco@Pro-Bacon.local>
2014-10-25 08:37:05 -0400

    Initial Commit

---
+++ BNB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BNB
+//
+//  Created by Francisco Hui on 10/25/14.
+//  Copyright (c) 2014 Francisco Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
